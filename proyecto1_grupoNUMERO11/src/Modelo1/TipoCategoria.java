34f1dfda92e6930de5d21363be95f993267dff26
Jeycop Navarrete <jeycopnava90@gmail.com>
2024-07-08 22:02:56 -0500

    Update TipoCategoria.java

--- proyecto1_grupoNUMERO11/src/Modelo1/TipoCategoria.java
+++ proyecto1_grupoNUMERO11/src/Modelo1/TipoCategoria.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package modelo1;
-
-/**
- *
- * @author ivand
- */
+package modelo;
 public enum TipoCategoria {
-    Ingreso,
-    Gasto
+    GASTO,
+    INGRESO
 }
