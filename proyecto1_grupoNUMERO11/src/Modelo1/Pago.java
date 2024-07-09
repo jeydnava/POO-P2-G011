71cd253095ed96f756dbb286c8adbc12b1761dcc
Jeycop Navarrete <jeycopnava90@gmail.com>
2024-07-08 21:56:17 -0500

    Update Pago.java

--- proyecto1_grupoNUMERO11/src/Modelo1/Pago.java
+++ proyecto1_grupoNUMERO11/src/Modelo1/Pago.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo1;
-
-/**
- *
- * @author ivand
- */
 public class Pago extends TransaccionFinanciera {
     private TipoAcredor acredor;
     private double interes;
